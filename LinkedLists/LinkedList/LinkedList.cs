762820ce254a284d15a0ad17f5c3b6d3187014d3
darruda <arruda.danielc@gmail.com>
2024-03-26 10:27:52 -0300

    Starting linked list class

---
+++ LinkedLists/LinkedList/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Arruda.LinkedList;
+
+public class LinkedListNodes
+{
+    public int Value { get; set; }
+    public LinkedListNodes? Next { get; set; }
+}
+
+public class LinkedList
+{
+    public List<LinkedListNodes> LinkedListCol { get; set; }
+
+    public LinkedList()
+    {
+        LinkedListCol = [];
+    }
+
+    public void Add(int value)
+    {
+        var newNode =
+            new LinkedListNodes() { Value = value, Next = null };
+
+        if (LinkedListCol.Count == 0)
+        {
+            LinkedListCol.Add(newNode);
+        }
+        else
+        {
+            var temp = LinkedListCol.Last();
+
+            temp.Next = newNode;
+        }
+    }
+
+    internal void WriteAll()
+    {
+        foreach (var node in LinkedListCol)
+        {
+            Console.Write($"Value: {node.Value} Next=> {node.Next?.Value}  |  ");
+        }
+    }
+}
---
+++ LinkedLists/Program.cs
@@ -0,0 +1,15 @@
+﻿using Arruda.LinkedList;
+namespace Training;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var linkedList = new LinkedList();
+
+        linkedList.Add(1);
+        linkedList.Add(2);
+        linkedList.Add(3);
+        linkedList.WriteAll();
+    }
+}
